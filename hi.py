d5fa85f97a7b15638dfd6a736504d1c53ae825ae
269042 <csbs269042@saranathan.ac.in>
2025-08-18 14:36:41 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def greet():
     """Return a custom friendly HTTP greeting."""
-    return "Welcome to My Awesome App!"
+    return "welcome to my page!"
 
 if __name__ == "__main__":
     app_host = "http://127.0.0.1:8080"
     print(f"App host link: {app_host}")
     app.run(host="127.0.0.1", port=8080, debug=True)
+
